beece956431b108e83b8982b840f0ab9b40a6fe8
vital-7-vital <vittaldasa777@gmail.com>
2026-03-02 18:40:49 +0530

    Add files via upload

---
+++ contactbook.py
@@ -0,0 +1,28 @@
+
+phone={}
+def add_cont():
+    name=input("enter the name")
+    contact=input("enter the contact")
+    phone[name]=contact
+def view_cont():
+    for k,v in phone.items():
+        print(k,v)
+def ser_ch():
+    sname=input("seach name: ")
+    if sname in phone:
+        print(sname,":",phone[sname])
+    else :
+        print("doesnt exist")
+e=1
+while(e==1):
+    choice=int (input("1 Add contact\n2 View contacts\n3 Search contact\n4 Exit"))
+    match choice:
+        case 1:
+            add_cont()
+        case 2:
+            view_cont()
+        case 3:
+            ser_ch()
+        case 4:
+            print("exit")
+            e=e+1
